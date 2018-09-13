a9afc0ddda5537872a5dd5a0443ee55c74010c77
Joseph Werle <joseph.werle@gmail.com>
2018-09-12 20:18:46 -0400

    refactor(index.js): Attempt to download all files

--- index.js
+++ index.js
@@ -365,7 +365,7 @@ async function start(argv) {
             files.length
           )
 
-          await Promise.all(pending.map(file => cfs.download(file)))
+          await Promise.all(files.map(file => cfs.download(file)))
 
           info(
             'Did sync identity archive for: "did:ara:%s"',
