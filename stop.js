0d6c066d61b69a9e10ad4a58043f10d6852f1353
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-11-27 18:39:34 -0500

    fix(): Linting fixes

--- stop.js
+++ stop.js
@@ -2,7 +2,6 @@ const { getInstance, setInstance } = require('./instance')
 const { warn } = require('ara-console')('identity-archiver')
 
 async function stop() {
-
   const channel = await getInstance()
 
   warn('Stopping identity archiver network node.')
